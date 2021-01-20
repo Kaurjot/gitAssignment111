501371ed1224410c2b8066bd77ba1f41b1fbef83
HARJOT KAUR <75960684+Kaurjot@users.noreply.github.com>
2021-01-20 10:18:17 -0500

    First Commit

--- app/src/main/java/com/example/gitassignment/MainActivity.java
+++ app/src/main/java/com/example/gitassignment/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // This is github integration testing --first commit
+        // This is github integration testing --first commit 11
     }
 }
